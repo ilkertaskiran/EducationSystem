51b4ac540280c29c2a4bf575b5a458a536ea5481
Kaan Taşkıran <kaan_taskiran@hotmail.com>
2023-01-08 20:22:20 +0300

    web project creating and configuration operations

---
+++ EducationSystem.WEB/Controllers/EducationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationSystem.WEB.Controllers
+{
+    public class EducationController : ControllerBase
+    {
+        private readonly ILogger<EducationController> _logger;
+
+        public EducationController(ILogger<EducationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetList()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
